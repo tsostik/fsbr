5cd2c84b46df85f4822496adeb5540303a746647
Konstantin Rabkin <tsostik@mail.ru>
2023-05-26 21:30:12 +0100

    Fix senior age according to EBL rules

--- src/interface.py
+++ src/interface.py
@@ -214,7 +214,7 @@ class Player:
                 if age < 26:
                     self.categories.append('J')
                     self.isJ = True
-                if age >= 62:
+                if age >= 63:
                     # This age should be reviewed later according to EBL policies.
                     # See SCoC for current european Championship
                     # http://www.eurobridge.org/wp-content/uploads/2017/10/EBL-info-corrective-letter-Senior-Age-121017.pdf
